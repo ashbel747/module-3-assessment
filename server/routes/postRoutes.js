364a8236ead51f937fed431b7fc97f58e6505e5b
Ashbel <kashbel747@gmail.com>
2025-08-02 21:16:18 +0300

    Add route controllers for modifying personal blog posts

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPosts } = require('../controllers/postController');
+const { createPost, updatePost, deletePost, getPosts } = require('../controllers/postController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+router.post('/', authMiddleware, createPost);
+router.put('/:id', authMiddleware, updatePost);
+router.delete('/:id', authMiddleware, deletePost);
 router.get('/', getPosts);
 
 module.exports = router;
