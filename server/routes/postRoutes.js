0e886d1a1eb46b56b65b4d19cc8f18f20daf21bf
Ashbel <kashbel747@gmail.com>
2025-08-03 12:47:19 +0300

    Feat: Add the backend controllers and routes to send res of singleposts and add comments

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { createPost, updatePost, deletePost, getPosts } = require('../controllers/postController');
+const { createPost, updatePost, deletePost, getPosts, getPostById, addCommentToPost } = require('../controllers/postController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+//Private
 router.post('/', authMiddleware, createPost);
 router.put('/:id', authMiddleware, updatePost);
 router.delete('/:id', authMiddleware, deletePost);
+router.post('/:id/comments', authMiddleware, addCommentToPost);
+
+//Public
 router.get('/', getPosts);
+router.get('/:id', getPostById);
 
 module.exports = router;
